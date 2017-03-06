35dbeefa2043ce7a3eb1b5507fdb48ea1defb5f8
Alexander Taxter <Sharquo@gmail.com>
2017-03-06 10:20:20 +0000

    Renamed some files and added a lost networking project.

---
+++ unity_mpNetworking/Assets/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class PlayerController : NetworkBehaviour
+{ 
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+
+    void Update ()
+    {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            CmdFire();
+        }
+
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+	}
+
+    [Command]
+    void CmdFire()
+    {
+        // Create a bullet from the bullet prefab
+        var bullet = (GameObject)Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation);
+
+        // Add veloctiy to the bullet
+        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 6;
+
+        NetworkServer.Spawn(bullet);
+
+        // Destroy the bullet after 2 seconds
+        Destroy(bullet, 2.0f);
+    }
+
+    public override void OnStartLocalPlayer()
+    {
+        GetComponent<MeshRenderer>().material.color = Color.blue;
+    }
+}
